55292797bc443fe1ee9c028fb6b9993b52ab8d4f
Michael Marinez <80479622+MixedMachine@users.noreply.github.com>
2023-09-14 15:57:41 -0600

    adding comm between devices (udp) & metrics

--- go.mod
+++ go.mod
@@ -2,12 +2,20 @@ module github.com/mixedmachine/device-finding
 
 go 1.20
 
-require github.com/grandcat/zeroconf v1.0.0
+require (
+	github.com/grandcat/zeroconf v1.0.0
+	github.com/shirou/gopsutil v3.21.11+incompatible
+)
 
 require (
 	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/miekg/dns v1.1.27 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
 	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
-	golang.org/x/sys v0.0.0-20190924154521-2837fb4f24fe // indirect
+	golang.org/x/sys v0.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,18 +4,26 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"os"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/grandcat/zeroconf"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+	snet "github.com/shirou/gopsutil/net"
 )
 
+// DeviceManager keeps track of active devices
 type DeviceManager struct {
 	activeDevices map[string]*zeroconf.ServiceEntry
 	mutex         sync.Mutex
 }
 
+// Add a new device to the list of active devices
 func (dm *DeviceManager) AddDevice(device *zeroconf.ServiceEntry) {
 	dm.mutex.Lock()
 	defer dm.mutex.Unlock()
@@ -28,6 +36,7 @@ func (dm *DeviceManager) AddDevice(device *zeroconf.ServiceEntry) {
 	dm.activeDevices[device.Instance] = device
 }
 
+// Remove inactive devices from the list of active devices
 func (dm *DeviceManager) RemoveInactiveDevice(newEntries map[string]*zeroconf.ServiceEntry) {
 	dm.mutex.Lock()
 	defer dm.mutex.Unlock()
@@ -44,7 +53,7 @@ func (dm *DeviceManager) RemoveInactiveDevice(newEntries map[string]*zeroconf.Se
 func registerService(service, domain string, stop chan struct{}) {
 	server, err := zeroconf.Register(
 		service,                     // service instance name
-		"_http._tcp",                // service type
+		"_myudp._udp",               // service type
 		domain,                      // service domain
 		8080,                        // service port
 		[]string{"txtv=0", "lo=la"}, // service metadata
@@ -91,6 +100,165 @@ func discoverServices(service, domain string, dm *DeviceManager) {
 	}
 }
 
+// Get device metrics
+func getDeviceMetrics() string {
+	var metrics []string
+
+	// Get CPU Usage (over a 1-second interval)
+	percentages, err := cpu.Percent(time.Second, false)
+	if err != nil {
+		return ""
+	}
+
+	// For simplicity, average the percentages if there are multiple CPUs
+	var totalPercent float64
+	for _, percent := range percentages {
+		totalPercent += percent
+	}
+	avgPercent := totalPercent / float64(len(percentages))
+	metrics = append(metrics, strconv.FormatFloat(avgPercent, 'f', 2, 64))
+
+	// Get Memory Info
+	memInfo, err := mem.VirtualMemory()
+	if err != nil {
+		return ""
+	}
+	metrics = append(metrics, strconv.FormatUint(memInfo.Available, 10), strconv.FormatUint(memInfo.Total, 10))
+
+	// Get Network Info
+	netInfo, err := snet.IOCounters(false)
+	if err != nil {
+		return ""
+	}
+
+	for _, netStat := range netInfo {
+		metrics = append(metrics, strconv.FormatUint(netStat.BytesSent, 10), strconv.FormatUint(netStat.BytesRecv, 10))
+	}
+
+	// Construct CSV-like string
+	csvMetrics := strings.Join(metrics, ",")
+
+	return csvMetrics
+}
+
+// Handle received message
+func handleReceivedMessage(self, msg string) {
+	protocol := strings.Split(msg, " ")
+	if len(protocol) != 4 {
+		fmt.Println("Invalid protocol")
+		return
+	}
+
+	device := protocol[0]
+	deviceIP := protocol[1]
+	msgType := protocol[2]
+	data := protocol[3]
+
+	fmt.Printf("Received message from %s: %s %s\n", device, msgType, data)
+	if msgType == "REQ" {
+		switch data {
+		case "metrics":
+			metrics := getDeviceMetrics()
+			conn, err := net.Dial("udp", fmt.Sprintf("%s:4256", deviceIP))
+			defer conn.Close()
+			if err != nil {
+				fmt.Println("Failed to dial UDP:", err)
+				return
+			}
+
+			_, err = conn.Write(buildMessage(self, getIPv4Address(), "RES", metrics))
+			if err != nil {
+				fmt.Println("Failed to write to UDP:", err)
+			}
+		default:
+			fmt.Println("Unknown request")
+		}
+	}
+}
+
+// Listen for devices
+func listenForDevices(self string) {
+	addr := net.UDPAddr{
+		IP:   net.ParseIP("0.0.0.0"),
+		Port: 4256,
+	}
+
+	conn, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		log.Fatalf("Failed to listen on UDP: %v", err)
+	}
+
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+		n, _, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			log.Fatalf("Failed to read from UDP: %v", err)
+		}
+		handleReceivedMessage(self, string(buf[:n]))
+	}
+}
+
+func buildMessage(self, selfIP, msgType, msg string) []byte {
+	return []byte(fmt.Sprintf("%s %s %s %s", self, selfIP, msgType, msg))
+}
+
+// Get devices metrics
+func getDevicesMetrics(self string, dm *DeviceManager, stop chan struct{}) {
+	for {
+		dm.mutex.Lock()
+		wg := &sync.WaitGroup{}
+		for _, device := range dm.activeDevices {
+			wg.Add(1)
+			go func(device *zeroconf.ServiceEntry) {
+				defer wg.Done()
+				if device.Instance == self {
+					return
+				}
+				if len(device.AddrIPv4) == 0 {
+					fmt.Println("No IPv4 address found for device", device.Instance)
+					return
+				}
+				url := fmt.Sprintf("%s:4256", device.AddrIPv4[0])
+
+				conn, err := net.Dial("udp", url)
+				if err != nil {
+					fmt.Println("Failed to dial UDP:", err)
+					return
+				}
+
+				_, err = conn.Write(buildMessage(self, getIPv4Address(), "REQ", "metrics"))
+				if err != nil {
+					fmt.Println("Failed to write to UDP:", err)
+				}
+				conn.Close()
+			}(device)
+		}
+		dm.mutex.Unlock()
+		wg.Wait() // Wait for all goroutines to finish
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func getIPv4Address() string {
+	addresses, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+
+	for _, address := range addresses {
+		ipnet, ok := address.(*net.IPNet)
+		if !ok || ipnet.IP.IsLoopback() || ipnet.IP.To4() == nil {
+			continue
+		}
+
+		return ipnet.IP.String()
+	}
+
+	return ""
+}
+
 func main() {
 	domain := "local."
 	serviceName, _ := os.Hostname()
@@ -105,10 +273,17 @@ func main() {
 
 	go registerService(serviceName, domain, stop)
 
+	go listenForDevices(serviceName)
+
 	time.Sleep(2 * time.Second)
 
-	go discoverServices("_http._tcp", domain, deviceManager)
+	go discoverServices("_myudp._udp", domain, deviceManager)
 
-	fmt.Println("Zeroconf initialized.Observing...")
+	fmt.Println("Zeroconf initialized. Observing...")
+
+	time.Sleep(2 * time.Second)
+
+	go getDevicesMetrics(serviceName, deviceManager, stop)
+
 	<-stop
 }
